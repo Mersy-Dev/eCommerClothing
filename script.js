b7e2f497041e0fd1f2754c14029cc087b4c100f7
Mersy-Dev <anuoluwafalaju@gmail.com>
2023-08-25 12:11:42 +0100

    Completed Singlepage clothing site

--- script.js
+++ script.js
@@ -51,7 +51,17 @@ const swiper = new Swiper('.swiper', {
     pagination:{
         el: '.swiper-pagination',
     },
+})
 
-   
+// show Search 
+const searchButton = document.querySelector('.t-search'),
+    tClose = document.querySelector('.search-close'),
+    showClass = document.querySelector('.site');
 
+
+searchButton.addEventListener('click', function(){
+    showClass.classList.toggle('showsearch')
+})
+tClose.addEventListener('click', function(){
+    showClass.classList.remove('showsearch')
 })
